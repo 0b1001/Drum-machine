3015f7056f4a3bc07c4793fd8b90298b255f7af7
Alex <korotkov9405co@gmail.com>
2023-07-17 13:49:20 +0300

    Final version

--- src/components/DrumMachine.js
+++ src/components/DrumMachine.js
@@ -4,17 +4,26 @@ import DrumPad from "./DrumPad";
 import { tracks } from "../data/tracks";
 
 const DrumMachine = () => {
-    const [keys, setKeys] = useState([
+    const keys =[
         "Q","W","E",
         "A","S","D",
         "Z","X","C",
-    ]);
+    ];
+
+    const [currentTrack, setCurrentTrack] = useState();
+
+    const drumPudClick = (track, audioRef) => {
+        setCurrentTrack(track);
+        audioRef.current.currentTime = 0;
+        audioRef.current.play();
+    }
+
     return (
         <div className="drum-machine" id="drum-machine">
-            <Display>Track</Display>
+            <Display>{currentTrack?.title}</Display>
             <div className="drum-machine-pads">
                 {keys.map((key, index) => 
-                    <DrumPad track={tracks[index]}>{key}</DrumPad>
+                    <DrumPad eventHandler={drumPudClick} track={tracks[index]} key={key} char={key}/>
                 )}
             </div>
         </div>
--- src/components/DrumPad.jsx
+++ src/components/DrumPad.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useEffect, useRef } from "react";
 
 const DrumPad = (props) => {
+    const audioRef = useRef();
+
+    const playAudio = () => {
+        props.eventHandler(props.track, audioRef);
+        audioRef.current.parentElement.className = 'drum-pad active';
+        setTimeout(() => {
+            audioRef.current.parentElement.className = 'drum-pad';
+        }, audioRef.current.duration * 1000);
+    };
+
+    useEffect(() => {
+        const keyDownHandler = (e) => {
+            if (e.key === props.char.toLowerCase() || e.key === props.char.toUpperCase()) {
+                playAudio();
+            }
+        };
+
+        document.addEventListener("keydown", keyDownHandler);
+
+        return () => {
+            document.removeEventListener("keydown", keyDownHandler);
+        };
+    });
+
     return (
-        <div className="drum-pad" id={props.track.title}>
-            <audio src={props.track.src} className='clip' id={props.children}></audio>
-            {props.children}
+        <div
+            className="drum-pad"
+            id={props.track.title}
+            onClick={() => playAudio()}
+        >
+            <audio
+                src={props.track.src}
+                className="clip"
+                id={props.char}
+                ref={audioRef}
+            ></audio>
+            <div>{props.char}</div>
         </div>
     );
 };
